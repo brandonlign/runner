7fde4411128046483783fb6079267b433a2a5df6
Brandon Li <brandon.li.gn@gmail.com>
2026-08-03 16:29:32 -0400

    Add renderer validation page

---
+++ mog_reference_face_validation/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { IdealReferenceFace } from "@/components/methodology/ideal-reference-face-natural";
+
+export default function Page() {
+  return (
+    <main>
+      <IdealReferenceFace />
+    </main>
+  );
+}
