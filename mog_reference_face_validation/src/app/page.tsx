92ea30fc9d1ea163d90216476580e467f252230d
Brandon Li <brandon.li.gn@gmail.com>
2026-08-03 16:53:03 -0400

    Render the third-generation reference portrait

--- mog_reference_face_validation/src/app/page.tsx
+++ mog_reference_face_validation/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { IdealReferenceFace } from "@/components/methodology/ideal-reference-face-natural";
+import { IdealReferenceFace } from "@/components/methodology/ideal-reference-face-v3";
 
 export default function Page() {
   return (
