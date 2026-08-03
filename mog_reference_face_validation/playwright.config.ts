d8a5920a3e9cc41694d611764818a280d099d7e1
Brandon Li <brandon.li.gn@gmail.com>
2026-08-03 16:30:50 -0400

    Add visual validation config

---
+++ mog_reference_face_validation/playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "./tests",
+  testMatch: "visual.spec.ts",
+  use: {
+    baseURL: "http://127.0.0.1:4173",
+    viewport: { width: 1440, height: 1200 },
+    screenshot: "only-on-failure",
+  },
+  webServer: {
+    command: "npm run start -- -p 4173",
+    url: "http://127.0.0.1:4173",
+    reuseExistingServer: false,
+    timeout: 120_000,
+  },
+});
